1797d922e8c32dd9a253663de6c7c9d24986a8fd
Enfis123 <toncenose123@gmail.com>
2023-08-16 20:22:15 -0500

    First

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+app.use(fileUpload());
+
+// Configuración de cabeceras CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.post('/upload', (req, res) => {
+  let EDFile = req.files.file;
+
+  EDFile.mv(`./files/${EDFile.name}`, err => {
+    if (err) return res.status(500).send({ message: err });
+
+    return res.status(200).send({ message: 'File uploaded' });
+  });
+});
+
+app.listen(3000, () => console.log('Server running'));
+
